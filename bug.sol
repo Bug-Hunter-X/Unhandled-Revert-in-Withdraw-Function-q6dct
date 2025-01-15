6ac58f231ec88903ee2d3c76686ced18f4957735
BugHunter-X <askzenai@gmail.com>
2025-01-15 06:36:05 +0100

    Add bug.sol file

---
+++ bug.sol
@@ -0,0 +1,9 @@
+```solidity
+function withdraw() public {
+    uint balance = balanceOf[msg.sender];
+    require(balance > 0, "Insufficient balance");
+    (bool success, ) = msg.sender.call{value: balance}(new bytes(0));
+    require(success, "Transfer failed.");
+    balanceOf[msg.sender] = 0;
+}
+```
